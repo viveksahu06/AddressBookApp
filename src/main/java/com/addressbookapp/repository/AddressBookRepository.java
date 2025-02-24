587d25d739809ae4235d54345e8653003333991b
viveksahu06 <viveksahu1006@gmail.com>
2025-02-24 12:28:22 +0530

    [Vivek] Add. Repository and database dependency added

--- src/main/java/com/addressbookapp/repository/AddressBookRepository.java
+++ src/main/java/com/addressbookapp/repository/AddressBookRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressBookRepository extends JpaRepository<AddressBook ,Long> {
+public interface AddressBookRepository extends JpaRepository<AddressBook ,Long>{
 }
